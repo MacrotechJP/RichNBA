0a57d92eecfb02150b6b83253fdf224bcb7ce751
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-10-03 22:18:16 +0900

    10/3
    チームホバー時の動画切り替え

--- app/assets/javascripts/player_video.js
+++ app/assets/javascripts/player_video.js
@@ -40,19 +40,16 @@ var ytWidth = 640;
 var ytHeight = 313;
 // 各動画情報
 var ytData = [
-    {
-        id: 'oX2H8vgs4Bo',
-        area: 'sample01'
-    }, {
-        id: 'wbqOCoBkGvg',
-        area: 'sample02'
-    }, {
-        id: 'idIHKr4GvPY',
-        area: 'sample03'
-    }, {
-        id: 'ke8aMAvP7pk',
-        area: 'sample04'
-    }
+  {id: 'wbqOCoBkGvg',area: 'teamvideo_Celtics'}, {id: 'K0vdUSOge4Y',area: 'teamvideo_Nets'}, {id: 'wbqOCoBkGvg',area: 'teamvideo_Knicks'}, 
+    {id: 'wbqOCoBkGvg',area: 'teamvideo_76ers'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Raptors'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Bulls'},
+      {id: 'wbqOCoBkGvg',area: 'teamvideo_Cavaliers'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Pistons'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Pacers'},
+        {id: 'wbqOCoBkGvg',area: 'teamvideo_Bucks'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Hawks'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Hornets'},
+          {id: 'wbqOCoBkGvg',area: 'teamvideo_Heat'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Magic'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Wizards'},
+  {id: 'wbqOCoBkGvg',area: 'teamvideo_Mavericks'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Rockets'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Grizzlies'},
+    {id: 'wbqOCoBkGvg',area: 'teamvideo_Pelicans'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Spurs'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Nuggets'},
+      {id: 'wbqOCoBkGvg',area: 'teamvideo_Timberwolves'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Thunder'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Blazers'},
+        {id: 'wbqOCoBkGvg',area: 'teamvideo_Jazz'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Warriors'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Clippers'},
+          {id: 'wbqOCoBkGvg',area: 'teamvideo_Lakers'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Suns'},{id: 'wbqOCoBkGvg',area: 'teamvideo_Kings'}
 ];
  
 // 各プレーヤーの埋め込み
@@ -105,30 +102,30 @@ function onPlayerStateChange(event) {
 
 
 //選手ホバー時、画像・動画変更
-$(function(){
+// $(function(){
 
-  var player_image1 = {Nets54: 'https://clutchpoints.com/wp-content/uploads/2019/09/Brooklyn-to-give-away-10000-Kyrie-Irving-jerseys-at-Oct.-25-game-vs.jpg',Warriors43:'https://d.newsweek.com/en/full/1237055/gettyimages-1053195938.jpg'};
-  var player_image3 = {Nets54: 'https://clutchpoints.com/wp-content/uploads/2019/07/Boston_wanted_to_offer_Kyrie_Irving_an_extension_last_summer_but_knew_he_wouldn_t_take_it_because_rules_capped_the_salary.jpg'};
-  var image1_1_50 = ["aa","aa","","","","","","","","","","","","","","","","","","","","","","","","","","","","",
-                     "","","","","","","","","","","","","","","","","","","","","","","","","","","",];
-  var image1_51_100 = [];
-  var image2_1_50 = [];
-  var image2_51_100 = [];
+//   $(".main_top_left_team").hover(function(){
+//     console.log("hover");
+//   },function(){
 
-  $(".main_top_right_team_player_left_center_star").hover(function() {
-    var id = $(this).find("h3").attr("id");
-    console.log($(this).find("h3").attr("id").replace(/[^0-9]/g, ''));
-    $(".swiper-slide").find('img[alt="Swiper01"]').attr("src",player_image1[id]);
-    // $(".swiper-slide").find('iframe').attr("src","https://www.youtube.com/embed/E6PER1dmIJU?controls=0&showinfo=0&disablekb=1&rel=0&enablejsapi=1&origin=http%3A%2F%2Flocalhost%3A3000&widgetid=1");
-    $(".swiper-slide").find('img[alt="Swiper03"]').attr("src",player_image3[id]);
-    $("#sample01").hide();
-    $("#sample02").show();
-  }, function() {
-    // $(".swiper-slide").find('img[alt="Swiper01"]').attr("src","https://www.youtube.com/embed/E6PER1dmIJU?controls=0&showinfo=0&disablekb=1&rel=0&enablejsapi=1&origin=http%3A%2F%2Flocalhost%3A3000&widgetid=1");
-    // $(".swiper-slide").find('img[alt="Swiper03"]').attr("src","http://bit.ly/2ojmdiy")
-  });
-  //https://www.youtube.com/embed/E6PER1dmIJU?controls=0&showinfo=0&disablekb=1&rel=0&enablejsapi=1&origin=http%3A%2F%2Flocalhost%3A3000&widgetid=1
-  //https://www.youtube.com/embed/E6PER1dmIJU
+//   });
+//   $(".main_top_right_team").hover(function(){
+//     console.log("hover");
+//   },function(){
+
+//   });
+
+//   $(".main_top_right_team_player_left_center_star").hover(function() {
+//     var id = $(this).find("h3").attr("id");
+//     console.log($(this).find("h3").attr("id").replace(/[^0-9]/g, ''));
+//     $(".swiper-slide").find('img[alt="Swiper01"]').attr("src",'https://00m.in/judFZ');
+//     $(".swiper-slide").find('img[alt="Swiper03"]').attr("src","https://00m.in/KVTo6");
+//     $("#sample01").hide();
+//     $("#sample02").show();
+//   }, function() {
+    
+//   });
   
-})
+  
+// })
 
