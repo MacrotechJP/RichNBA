1b90e02e4dc02b26f6f6c0be99f7c2c43d045c60
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-10-20 22:12:16 +0900

    10/20
    スクレイピング

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,36 +1,67 @@
 class Scraping
 
-  # for num in 1..3 do
+  ## Stage商品一覧より各商品URLをDBに保存
+  # nexturl = "https://www.x-stage2.jp/product-list?page=1"
+  # while true do
   #   agent = Mechanize.new
-  #   page = agent.get("https://jordan.co.jp/?mode=srh&sort=n&cid=&keyword=")
-  #   element = page.search('.imgBox a')
-  #   if element.size() > 0 then
-  #     puts "存在する"
+  #   page = agent.get(nexturl)
+  #   element = page.search('.to_next_page.pager_btn')[0]
+  #   if element.nil? then
+  #     break
   #   else
-  #     puts "存在しない"
+  #     elementimage = page.search('.item_data a')
+  #     elementprice = page.search('.selling_price span')
+  #     elementimage.zip(elementprice).each do |eleimage, eleprice|
+  #       Item.create(siteurl:eleimage.get_attribute(:href),price:eleprice.inner_text.gsub(",","").gsub("円",""),ecsite_id:4)
+  #     end
+        
+  #     nexturl = "https://www.x-stage2.jp"+element.get_attribute(:href)
   #   end
-  #   puts element.size()
   # end
-    
-  # puts Item.all.siteurl
-
+  ## Stage各商品URLの画像を複数保存
   # Item.all.each do |item|
+
+  #   agent = Mechanize.new
+  #   page = agent.get(item.siteurl)
+  #   element = page.search('.slider_image_box img')
+  #   element.each do |ele|
+  #     puts ele.get_attribute(:src)
+  #     Itemimage.create(url:ele.get_attribute(:src),item_id:item.id)
+  #   end
+    
+  # end
+
+
+  ## BB KONG商品一覧より各商品URLをDBに保存
+  # nexturl = "https://www.bbkong.net/fs/alleyoop/GoodsSearchList.html?pageno=1"
+  # while true do
+  #   agent = Mechanize.new
+  #   page = agent.get(nexturl)
+  #   element = page.search('.FS2_pager_link_next')[0]
+  #   if element.nil? then
+  #     break
+  #   else
+  #     elementimage = page.search('.FS2_thumbnail_container a')
+  #     elementprice = page.search('.itemPrice')
+  #     elementimage.zip(elementprice).each do |eleimage, eleprice|
+  #       Item.create(siteurl:eleimage.get_attribute(:href),price:eleprice.inner_text.gsub(",","").gsub("円",""),ecsite_id:11)
+  #     end
+        
+  #     nexturl = "https://www.bbkong.net/fs/alleyoop/GoodsSearchList.html"+element.get_attribute(:href)
+  #   end
+  # end
+  ## BB KONG各商品URLの画像を複数保存
+  Item.all.each do |item|
+
     agent = Mechanize.new
-    page = agent.get(Item.find(9).siteurl)
-    element = page.search('body')
+    page = agent.get(item.siteurl)
+    element = page.search('.FS2_additional_image_thumbnail_container img')
     element.each do |ele|
       puts ele.get_attribute(:src)
+      Itemimage.create(url:ele.get_attribute(:src),item_id:item.id)
     end
-    puts element
     
+  end
 
-    # puts item.siteurl
-  # end
-
-  
-    # element.each_with_index do |ele,i|
-    #   Item.create(url:"https://jordan.co.jp/"+ele.get_attribute(:href))
-    #   puts "#{i}:https://jordan.co.jp/#{ele.get_attribute(:href)}"
-    # end
     
 end
