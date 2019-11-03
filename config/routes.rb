88bde4dd9de1217be81a0b159b835a1404e2c62d
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-11-03 19:45:52 +0900

    11/3
    検索修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "item#index"
-  post "/search" ,to:"item#search"
+  get "/search" ,to:"item#search"
   get "/info" ,to:"infomation#index"
   get "/mypage" ,to:"mypage#index"
   get "/mypage/checkitem" ,to:"mypage#checkitem"
