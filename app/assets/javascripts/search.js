4f98ace45471aa8f001263deffd349c5c04e9e9d
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-11-12 23:39:02 +0900

    11/12
    商品検索ページview作成

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,48 @@
+$(function(){
+  $('.search_result_head_sort').each(function() {
+    $(this).children('select').css('display', 'none');
+    
+    var $current = $(this);
+    
+    $(this).find('option').each(function(i) {
+      if (i == 0) {
+        $current.prepend($('<div>', {
+          class: $current.attr('class').replace(/search_result_head_sort/g, 'sel__box')
+        }));
+        
+        var placeholder = $(this).text();
+        $current.prepend($('<span>', {
+          class: $current.attr('class').replace(/search_result_head_sort/g, 'sel__placeholder'),
+          text: placeholder,
+          'data-placeholder': placeholder
+        }));
+        
+        return;
+      }
+      
+      $current.children('div').append($('<span>', {
+        class: $current.attr('class').replace(/search_result_head_sort/g, 'sel__box__options'),
+        text: $(this).text()
+      }));
+    });
+  });
+  
+  // Toggling the `.active` state on the `.sel`.
+  $('.search_result_head_sort').click(function() {
+    $(this).toggleClass('active');
+  });
+  
+  // Toggling the `.selected` state on the options.
+  $('.sel__box__options').click(function() {
+    var txt = $(this).text();
+    var index = $(this).index();
+    
+    $(this).siblings('.sel__box__options').removeClass('selected');
+    $(this).addClass('selected');
+    
+    var $currentSel = $(this).closest('.search_result_head_sort');
+    $currentSel.children('.sel__placeholder').text(txt);
+    $currentSel.children('select').prop('selectedIndex', index + 1);
+  });
+  
+})
